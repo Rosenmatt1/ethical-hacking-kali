5e68f326aced3dbdaa1d4f85eaffc0f1031cb1bb
Rosenmatt1 <rosenmatt1@gmail.com>
2020-12-30 22:52:18 -0500

    Fixed code error tht had missing space

--- mac_changer.py
+++ mac_changer.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
-import subprocess
+import subprocess # the true allows to run Linux commands through function
+import optparse
+#allows to use users inputs as arguments
 
-# raw_input() for python2 to jut use commnd python
+# raw_input() for python2
 interface = input("interface > ")
 # "wlan0"
 new_mac = input("new MAC > ")
-# "00:1:22:33:44:77"
+# "00:11:22:33:44:77"
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface " down", shell=True)  # the true allows to run Linux commands through function
+subprocess.call("ifconfig " + interface + " down", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + "up", shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
-subprocess.call(["ifconfig", interface, "down"])
-# a list that tells the order of commands allowed by the user to pevent the user input from running other commands
+# subprocess.call(["ifconfig ", interface, " down"])
+# subprocess.call(["ifconfig ", interface, " hw ether ", + new_mac, shell=True])
+# subprocess.call(["ifconfig ", interface, " up"])
+# a list that tells the order of commands allowed by the user to prevent the user input from running other commands
