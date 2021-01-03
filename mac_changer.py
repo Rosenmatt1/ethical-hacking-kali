b9bdba10f67924cebb7ee931864bdb71fb69f13c
Rosenmatt1 <rosenmatt1@gmail.com>
2021-01-03 02:35:05 -0500

    Added if statements in the get_argumnets function to exit the functio if user does not provide an interface or mac address

--- mac_changer.py
+++ mac_changer.py
@@ -3,7 +3,6 @@
 import subprocess # the true allows to run Linux commands through function
 import optparse
 #allows to use users inputs as arguments
-#parser
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -11,7 +10,12 @@ def get_arguments():
     parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
     # python mac_changer.py --interface wlan0 --mac 00:11:22:33:44:55
     # python mac_changer.py --i wlan0 --m 00:11:22:33:44:55
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an new mac, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -24,5 +28,5 @@ def change_mac(interface, new_mac):
     # subprocess.call(["ifconfig ", interface, " up"])
     # a list that tells the order of commands allowed by the user to prevent the user input from running other commands
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
