302a094cd0f9e0118f80d7cb93d4a272ba900f0c
Rosenmatt1 <rosenmatt1@gmail.com>
2021-01-06 01:53:39 -0500

    Put code int new function get_current_mac

--- mac_changer.py
+++ mac_changer.py
@@ -28,13 +28,16 @@ def change_mac(interface, new_mac):
     # subprocess.call(["ifconfig ", interface, " up"])
     # a list that tells the order of commands allowed by the user to prevent the user input from running other commands
 
+
+def get_current_mac():
+    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        print(mac_address_search_result.group(0))
+    else:
+        print("[-] Could not read MAC address")
+
 options = get_arguments()
 # change_mac(options.interface, options.new_mac)
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
 
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address")
+
