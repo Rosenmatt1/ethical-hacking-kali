622455aa36b8fcff49a9cf47cc87114018a20b05
Rosenmatt1 <rosenmatt1@gmail.com>
2021-01-18 23:34:21 -0500

    Added a function in the scanner.py file that returns all the MAC address of clients on the network

--- scanner.py
+++ scanner.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import subprocess # the true allows to run Linux commands through function
+import scapy.all as scapy
 
-ip_address = "192.168.1.1/24"
+def scan(ip):
+    scapy.arping(ip)
 
-subprocess.call("netdiscover " + "-r " + ip_address, shell=True)
+scan("10.0.2.1/24")
+
+
+
+# ip_address = "192.168.1.1/24"
+#
+# subprocess.call("netdiscover " + "-r " + ip_address, shell=True)
